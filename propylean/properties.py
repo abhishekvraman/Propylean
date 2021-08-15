992363f6892c4ad920bb8f74048ec415ebde20ac
Abhishek <avraman@uci.edu>
2021-08-15 15:25:08 +0530

    Updated properties classes with inheritance from a general class. Unit convertion features added for Pressure and Temperature. Rest of the properties to be added/updated when required

--- propylean/properties.py
+++ propylean/properties.py
@@ -1,31 +1,89 @@
-class Pressure:
+class _Property:
+    def __init__(self, value = None, unit= None):
+        self._value = value
+        self._unit = unit
+    
+    @property
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self, value):
+        self._value = value
+    def __repr__(self) -> str:
+        return str(self.value) + ' ' + self.unit
+    @property
+    def unit(self):
+        return self._unit
+    @unit.setter
+    def unit(self, unit):
+        self._unit = unit
+
+class Pressure(_Property):
     def __init__(self, value = 1, unit= 'Pa'):
-        self.value = value
-        self.unit = unit
-    
-    def __repr__(self) -> str:
-        return str(self.value) + ' ' + self.unit
+        super().__init__(value,unit) 
 
-class Temperature:
+    @_Property.unit.setter
+    def unit(self, unit):
+        try:
+            conversion_factors = {'atm': 101320,
+                                'bar': 100000,
+                                'psi': 6893,
+                                'kPa': 1000,
+                                'MPa': 1000000,
+                                'kg/cm^2': 98070,
+                                'ata': 98070,
+                                'Torr': 133.3,
+                                'mm Hg': 133.3,
+                                'in water':2490,
+                                'mm water': 9.81,
+                                'Pa': 1
+                                }
+            self._value =  conversion_factors[self._unit] * self._value / conversion_factors[unit]
+            self._unit = unit
+        except:
+            raise Exception('Selected unit is not supported or a correct unit of Pressure.')
+  
+
+class Temperature(_Property):
     def __init__(self, value = 1, unit= 'K'):
-        self.value = value
-        self.unit = unit
+        super().__init__(value,unit)
     
-    def __repr__(self) -> str:
-        return str(self.value) + ' ' + self.unit
+    @_Property.unit.setter
+    def unit(self, unit):
 
-class MassFlowRate:
+        if unit in ['K', 'C', 'F', 'R']:
+            while unit != self._unit:
+                if self._unit == 'K':
+                    self._value -= 273.15
+                    self._unit = 'C'
+                    if unit == self._unit:
+                        break
+                if self._unit == 'C':
+                    self._value = self._value * 9/5 + 32
+                    self._unit = 'F'
+                    if unit == self._unit:
+                        break
+                if self._unit == 'F':
+                    self._value = self._value  + 459.67
+                    self._unit = 'R'
+                    if unit == self._unit:
+                        break
+                if self._unit == 'R':
+                    self._value = self._value  * 5 / 9
+                    self._unit = 'K'
+                    if unit == self._unit:
+                        break
+        else:
+            raise Exception('Selected unit is not supported or a correct unit of Temperature.')
+
+class MassFlowRate(_Property):
     def __init__(self, value = 1, unit= 'Kg/s'):
-        self.value = value
-        self.unit = unit
-    
-    def __repr__(self) -> str:
-        return str(self.value) + ' ' + self.unit
+        super().__init__(value,unit)
 
-class Power:
+class VolumetricFlowRat(_Property):
+    def __init__(self, value = 1, unit= 'm^3/s'):
+        super().__init__(value,unit)
+
+class Power(_Property):
     def __init__(self, value = 1, unit= 'W'):
-        self.value = value
-        self.unit = unit
-    
-    def __repr__(self) -> str:
-        return str(self.value) + ' ' + self.unit
+        super().__init__(value,unit)
