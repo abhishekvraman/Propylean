d27bc5978f0d76a57c27f55441c767c720b5c7eb
Abhishek <avraman@uci.edu>
2021-09-05 12:22:53 +0530

    Updating the test to incorporate latest changes in EnergyStream class which inherits from Power property class

--- tests/test_streams/test_streams.py
+++ tests/test_streams/test_streams.py
@@ -3,7 +3,22 @@ import pandas as pd
 
 from propylean import streams
 
-# def test_EnergyStream_instantiation():
+def test_EnergyStream_instantiation():
+    energy = streams.EnergyStream()
+    assert energy.value == 0
+    assert energy.unit == 'W'
+    energy = streams.EnergyStream(10, 'MW')
+    assert energy.value == 10
+    assert energy.unit == 'MW'
+    with pytest.raises(Exception):
+        en = streams.EnergyStream(10, 'Barrels of oil per day')
+
+def test_EnergyStream_value_in_other_unit_getter():
+    energy = streams.EnergyStream(10, 'MW')
+    energy.unit = 'W'
+    assert energy.value == 10000000
+    energy.unit = 'BTU/h'
+    assert abs(energy.value- 34121416.331279) <0.0001
     
 
  
