b454f8bf9bac4e635a8c24b7952a0f55c23c5d80
Abhishek <avraman@uci.edu>
2021-10-03 17:52:52 +0530

    Adding listing of stream objects functionality to both stream classes

--- propylean/streams.py
+++ propylean/streams.py
@@ -1,29 +1,45 @@
 from thermo.chemical import Chemical
 import propylean.properties as prop  
 class EnergyStream (prop.Power):
+    items = [] 
     def __init__(self, value= 0, unit= 'W', tag= None, 
                  to_equipment_tag= None, from_equipment_tag= None):
                  super().__init__(value= value, unit=unit)
                  self.tag = tag
                  self.to_equipment_tag = to_equipment_tag
                  self.from_equipment_tag = from_equipment_tag
+
+                 EnergyStream.items.append(self)
     
     def __repr__(self) -> str:
-        return 'Energy Stream Tag: ' + self.tag         
+        return 'Energy Stream Tag: ' + self.tag  
+
+    @classmethod
+    def list_objects(cls):
+        return cls.items       
       
 class MaterialStream:
+    items = [] 
     def __init__(self, mass_flow_rate = 0,
                  pressure = 101325,
                  temperature = 298,
                  tag = None,
                  to_equipment_tag= None, from_equipment_tag= None):
+                 
                  self.tag = tag
                  self.mass_flow_rate = prop.MassFlowRate(mass_flow_rate)
                  self.pressure = prop.Pressure(pressure)
                  self.temperature = prop.Temperature(temperature)
                  self.molar_flow_rate = prop.MolarFlowRate()
                  self.to_equipment_tag = to_equipment_tag
+                 self.to_equipment_index = None
                  self.from_equipment_tag = from_equipment_tag 
-    
+
+                 MaterialStream.items.append(self)
+
+    @classmethod
+    def list_objects(cls):
+        return cls.items
+
     def __repr__(self) -> str:
         return 'Material Stream Tag: ' + self.tag
