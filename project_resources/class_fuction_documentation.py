bc76f0e17702efc71a5a52cbc5c1314ac2a4b789
Abhishek V Raman <abhishekvraman@gmail.com>
2021-12-04 18:53:21 +0530

    Adding sample documentation for all project classes and functions

---
+++ project_resources/class_fuction_documentation.py
@@ -0,0 +1,27 @@
+""" 
+    DESCRIPTION:
+        Xyz sdfcsf sfsdfs sfdsfssdf sfsfsf sfsdfs sfddsfs sdfsfdsvh
+        afafcd afadf afaf asfafe gtde  aedge egaav egza agegega.
+    
+    PARAMETERS:
+        argument_1:
+            Required: Yes/No
+            Type: int/float/list/dict...
+            Acceptable values: positive, [1,2,3],['bar', 'Pa']
+            Description: Abc eiwna qen awn
+    
+    RETURN VALUE:
+        Type: list/object/int/dataframe
+        Description: Dws sfas af
+    
+    ERROR RAISED:
+        Type:
+        Description:
+    
+    SAMPLE USE CASES:
+        >>>data = function(a=1,b=3)
+        >>>print(data)
+        data displayed as is or as per __repr__
+
+
+"""
