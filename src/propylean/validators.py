12383127d7df516397d5af1dbfa3232f57d118b3
Abhishek V Raman <abhishekvraman@gmail.com>
2022-08-13 15:06:51 +0530

    adding docstr

--- src/propylean/validators.py
+++ src/propylean/validators.py
@@ -1,5 +1,15 @@
 class _Validators(object):
-    def validate_arg_prop_value_type(self, arg_prop_name, value, correct_type):
-        if not isinstance(value, correct_type):
+    def validate_arg_prop_value_type(self, arg_prop_name, value, correct_types):
+        """
+        DESCRIPTION:
+            Function to validate values passed to properties or args is of
+            of correct types.
+        PARAMETERS:    
+            arg_prop_name:
+                Required: No
+                Type: String  
+                Description: Minimum flow requirement of the pump
+        """
+        if not isinstance(value, correct_types):
             raise Exception("""Incorrect type '{0}' of value provided to '{1}'. Should be '{2}'.
-            """.format(str(type(value)), arg_prop_name, str(correct_type)))
+            """.format(str(type(value)), arg_prop_name, str(correct_types)))
