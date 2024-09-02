ee07637951bd076d50a78bf7e7e1f01f3a5b55f2
abhishekvraman <abhishekvraman@gmail.com>
2024-09-03 00:07:23 +0530

    Series will not inherit.

--- src/propylean/series.py
+++ src/propylean/series.py
@@ -1,9 +1,10 @@
 from pandas import Series as PdSeries
+from pyspark.pandas import Series as SpkSeries
 from propylean.validators import _Validators
 from propylean.properties import _Property
 
-class Series(PdSeries):
-    def __init__(self, data, prop, unit=None, index=None, 
+class Series():
+    def __init__(self, data, prop, unit=None, index=None, is_spark=False,
                  dtype=None, name=None, copy=False, fastpath=False) -> None:
         """
         DESCRIPTION:
@@ -31,7 +32,26 @@ class Series(PdSeries):
                     Default value: NA
                     Description: Unit that the property of the series has. If not specified, default unit of
                                  the property is considered.
-
+                index:
+                    Required: No
+                    Type: Array type
+                    Description: Refer Pandas.series or pyspark.series documention.
+                dtype:
+                    Required: No
+                    Type: Array type
+                    Description: Refer Pandas.series or pyspark.series documention
+                name:
+                    Required: No
+                    Type: Array type
+                    Description: Refer Pandas.series or pyspark.series documention
+                copy:
+                    Required: No
+                    Type: Array type
+                    Description: Refer Pandas.series or pyspark.series documention
+                fastpath:
+                    Required: No
+                    Type: Array type
+                    Description: Refer Pandas.series or pyspark.series documention
                 
             RETURN VALUE:
                 Type: Series
@@ -42,17 +62,26 @@ class Series(PdSeries):
                 Description: 
             
             SAMPLE USE CASES:
-                >>>  class AwesomeMesuringDevice(_MeasuringInstruments):
-                >>>     def __init__(**kwargs):
-                >>>         some_property = 20
+                >>> from propylean import Series
+                >>> from propylean.properties import Length
+                >>> r = [1, 2]
+                >>> ser = Series(r, prop=Length copy=False)
             
         """
-        super().__init__(data=data, index=index, dtype=dtype, 
-                         name=name, copy=copy, fastpath=fastpath)
         self._prop = None
         self._unit = None
         self.prop = prop
-        self.unit = unit
+        self.unit = unit if unit is not None else prop().unit
+        if type(data) == PdSeries:
+            self.instance = data
+        elif type(data) == SpkSeries:
+            self.instance = data
+        elif is_spark:
+            self.instance = SpkSeries(data=data,index=index,dtype=dtype,
+                                      copy=copy,fastpath=fastpath)
+        else:
+            self.instance = PdSeries(data=data,index=index,dtype=dtype,
+                                      copy=copy,fastpath=fastpath)
     
     @property
     def prop(self):
@@ -61,4 +90,13 @@ class Series(PdSeries):
     def prop(self, value):
         _Validators.validate_child_class("prop", value, _Property, "physical or dimensionless property from propylean.properties")
         self._prop = value
-    
+    @property
+    def unit(self):
+        return self._unit
+    @unit.setter
+    def unit(self, value):
+        _Validators.validate_property_unit(self._prop, value)
+        self._unit = value
+
+    def __repr__(self) -> str:
+        return "Property: {}\nunit: {}\n".format(self._prop.__name__, self._unit) + str(self.instance)
